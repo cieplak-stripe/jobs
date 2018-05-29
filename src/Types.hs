325a017b4ae94e5a2b8c3e24809f95e45a12e9e1
patrick cieplak <patrick.cieplak@gmail.com>
2018-05-28 22:21:18 -0700

    Add lenses and json instances for application data types

--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TemplateHaskell        #-}
+{-# LANGUAGE DuplicateRecordFields  #-}
+{-# LANGUAGE DeriveGeneric          #-}
+{-# LANGUAGE DeriveAnyClass         #-}
+{-# LANGUAGE FlexibleInstances      #-}
 --------------------------------------------------------------------------------
 module Types where
 --------------------------------------------------------------------------------
-import Data.Lens
+import Control.Lens
 import Data.Aeson
 import GHC.Generics
 import Prelude ()
@@ -32,25 +40,25 @@ data TaskState
   | Ready
   | Running
   | Complete
-  deriving (Show, Eq, FromJSON, ToJSON)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON)
 --------------------------------------------------------------------------------
 -- |
 data Execution
   = Execution
   { __id          :: Int
   , __task_id     :: Int
-  , __started_at  :: Timestamp
+  , __started_at  :: Text
   , __finished_at :: Maybe Text
   , __state       :: ExecutionState
   , __stdout      :: Maybe Text
   , __stderr      :: Maybe Text
-  }
+  } deriving (Show, Eq, Generic)
 
 data ExecutionState
   = Started
   | Succeeded
   | Failed
-  deriving (Show, Eq, FromJSON, ToJSON)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON)
 --------------------------------------------------------------------------------
 -- |
 data Alert
@@ -59,4 +67,24 @@ data Alert
   , __execution_id :: Int
   , __message      :: Value
   } deriving (Show, Eq, Generic)
+--------------------------------------------------------------------------------
+--                                                                        Lenses
+makeFieldsNoPrefix ''Job
+makeFieldsNoPrefix ''Task
+makeFieldsNoPrefix ''Execution
+makeFieldsNoPrefix ''Alert
+--------------------------------------------------------------------------------
+--                                                                JSON Instances
+
+instance ToJSON   Job       where toJSON    = genericToJSON    defaultOptions {fieldLabelModifier = drop 2}
+instance FromJSON Job       where parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
+
+instance ToJSON   Task      where toJSON    = genericToJSON    defaultOptions {fieldLabelModifier = drop 2}
+instance FromJSON Task      where parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
+
+instance ToJSON   Execution where toJSON    = genericToJSON    defaultOptions {fieldLabelModifier = drop 2}
+instance FromJSON Execution where parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
+
+instance ToJSON   Alert     where toJSON    = genericToJSON    defaultOptions {fieldLabelModifier = drop 2}
+instance FromJSON Alert     where parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
 --------------------------------------------------------------------------------
