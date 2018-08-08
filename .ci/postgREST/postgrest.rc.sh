2e2f4c231a724b491760d0a891669bfb4a5cb599
Patrick Cieplak <patrick.cieplak@gmail.com>
2018-08-08 15:28:51 -0700

    Update postgrest.rc.sh

--- .ci/postgREST/postgrest.rc.sh
+++ .ci/postgREST/postgrest.rc.sh
@@ -3,29 +3,17 @@
 # $FreeBSD$
 #
 
-# PROVIDE: postgrest
+# PROVIDE: postgrestd
 # REQUIRE: LOGIN
 # KEYWORD: shutdown
 
-#
-# Add the following line to /etc/rc.conf to enable postgrest:
-# postgrest_enable (bool):  Set to "YES" by default.
-#                           Set it to "NO" to disable postgrest.
-
 . /etc/rc.subr
 
-name="postgrest"
-rcvar=postgrest_enable
+name="postgrestd"
+app="/usr/local/bin/postgrestd.sh"
+rcvar=postgrestd_enable
 load_rc_config $name
-: ${postgrest_enable="YES"}
-postgrest_user="postgREST"
-spidfile="/home/postgREST/${name}_daemon.pid"
-cpidfile="/home/postgREST/${name}_child.pid"
-logfile="/home/postgREST/${name}_daemon.log"
-procname="postgrest /etc/postgrest.conf"
 command="/usr/sbin/daemon"
-command_args="-P ${spidfile} -p ${cpidfile} -r "  '"'"${procname}"'"'  "-u ${postgrest_user} -t postgrest_service"
-pidfile="${cpidfile}"
-interpreter="/bin/sh"
-check_pidfile $pidfile $procname $interpreter
+daemon_user="user"
+command_args="-u ${daemon_user} -o /home/user/postgrestd.log -r ${app}"
 run_rc_command "$1"
